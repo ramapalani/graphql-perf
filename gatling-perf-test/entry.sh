299ebf546b43af593d1b5bb968c9fe6e8a77c8b1
rpalaniappan <rama_palaniappan@intuit.com>
2022-05-18 00:06:24 -0700

    fix gatling test

--- gatling-perf-test/entry.sh
+++ gatling-perf-test/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAVEN_OPTS="${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions \
+JAVA_OPTS="${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions \
   -XX:+UseG1GC \
   -XX:+UseStringDeduplication \
   -XX:MinRAMPercentage=50.0 \
@@ -8,7 +8,7 @@ MAVEN_OPTS="${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions \
   -XshowSettings:vm"
 
 # use app dir for tmp dir
-MAVEN_OPTS="${MAVEN_OPTS} -Djava.io.tmpdir=/app/tmp"
+JAVA_OPTS="${JAVA_OPTS} -Djava.io.tmpdir=/app/tmp"
 
 # Set these as environment values through container runtime
 MIN_USER_COUNT="${MIN_USER_COUNT:=1}"
@@ -16,17 +16,17 @@ TEST_RAMP_SECONDS="${TEST_RAMP_SECONDS:=1}"
 TOTAL_TPS="${TOTAL_TPS:=1}"
 TEST_DURATION_SECONDS="${TEST_DURATION_SECONDS:=60}"
 SINGLE_SUBGRAPH_100KB_LATENCY_MILLISECONDS="${SINGLE_SUBGRAPH_100KB_LATENCY_MILLISECONDS:=100}"
-GRAPHQL_URL=${GRAPHQL_URL:="http://apollo-gateway:4000/graphql"}
+#GRAPHQL_URL="${GRAPHQL_URL:=http://apollo-gateway:4000/graphql}"
 
 echo Env Values
 printenv
 
-mvn gatling:test \
-  -DGRAPHQL_URL=${GRAPHQL_URL} \
+exec java $JAVA_OPTS   \
   -DMIN_USER_COUNT=${MIN_USER_COUNT} \
   -DTEST_RAMP_SECONDS=${TEST_RAMP_SECONDS} \
   -DTOTAL_TPS=${TOTAL_TPS} \
   -DTEST_DURATION_SECONDS=${TEST_DURATION_SECONDS} \
-  -DSINGLE_SUBGRAPH_100KB_LATENCY_MILLISECONDS=${SINGLE_SUBGRAPH_100KB_LATENCY_MILLISECONDS}
+  -DSINGLE_SUBGRAPH_100KB_LATENCY_MILLISECONDS=${SINGLE_SUBGRAPH_100KB_LATENCY_MILLISECONDS} \
+  -jar /app/gatling-perf-test.jar \
+  -s supergraph.BasicSimulation
 
-#sleep 3600
